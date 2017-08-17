0f252171c3ba3a94b4bba88dbd5cf1add6e17392
Yuri Chaves <yurichaves94@gmail.com>
2017-08-17 10:04:34 -0300

    adicionei minhas informações

--- ufpa_projetos2_2017_abril.c
+++ ufpa_projetos2_2017_abril.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-		
+
 	puts("Esse programa \"apresenta\" a turma de 2017, 1o semestre");
 	puts("Matricula - Nome - Curriculo Lattes");
 	puts("A partir daqui cada um insere suas linhas, guardando uma linha em branco:\n");
@@ -13,5 +13,8 @@ int main() {
 
 	puts("201607040023 - João Pereira Paiva - http://lattes.cnpq.br/6282346015903573");
 	puts("Meu mantra: Não venci todas as batalhas que lutei mas perdi todas que deixei de lutar\n");
+
+	puts("201607040013 - Yuri Chaves Mesquita da Silva - http://lattes.cnpq.br/0098644462613252");
+	puts("Meu Mantra: Não sei diferenciar teimosia de persistência, esse é meu maoir defeito e qualidade\n");
 
 }
